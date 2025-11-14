c8385be04b5b1af9d5808c782b05f877321a90d9
Amadeu Behrend <amadeubehrend@gmail.com>
2025-11-14 11:56:36 -0800

    created user.js and auth.js files on server and implemented a User model to help with login and initial registration

---
+++ server/models/user.js
@@ -0,0 +1,51 @@
+const bcrypt = require("bcryptjs");
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    "User",
+    {
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true,
+        },
+      },
+
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [8], //,
+        }, // Implement and test later: is: Regex
+      },
+    },
+    {
+      //for security: hide password field from queries
+      defaultScope: {
+        attributes: { exclude: ["password"] },
+      },
+      scopes: {
+        //password only when explicitly requested
+        withPassword: {
+          attributes: {},
+        },
+      },
+    }
+  );
+
+  // Hash password before saving
+  User.beforeCreate(async (user, options) => {
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+  });
+
+  return User;
+};
